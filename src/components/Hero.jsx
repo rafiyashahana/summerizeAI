1c40e4511601b676f07271a797fc7b98261a0c1c
shahana <rafiyashahana@gmail.com>
2023-05-11 10:19:45 +0300

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { logo } from "../assets";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-19 pt-3">
+        <img
+          src={logo}
+          alt="summerizerai_logo"
+          className="w-28 object-contain"
+        />
+        <button
+          type="button"
+          onClick={() => {
+            window.open(
+              "https://github.com/rafiyashahana/summerizeAI",
+              "_blank"
+            );
+          }}
+          className="black_btn"
+        >
+          Github
+        </button>
+      </nav>
+    </header>
+  );
 };
 
 export default Hero;
