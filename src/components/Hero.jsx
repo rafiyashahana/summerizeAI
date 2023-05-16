aa4ca3f608464734e5f7f9c6f2ae9b61b1a1bf72
shahana <rafiyashahana@gmail.com>
2023-05-16 10:09:52 +0300

    added input for demo section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { logo } from "../assets";
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className="flex justify-between items-center w-full mb-19 pt-3">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
         <img
           src={logo}
           alt="summerizerai_logo"
@@ -22,6 +22,15 @@ const Hero = () => {
           Github
         </button>
       </nav>
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with SummarizerAI, an open-source article
+        summarizer that transforms lengthy articles into clear and concise
+        summaries.
+      </h2>
     </header>
   );
 };
