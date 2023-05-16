fe401d15c7a02a124f9da32f1be7df350a15f0a7
shahana <rafiyashahana@gmail.com>
2023-05-16 08:30:09 +0300

    completed Hero section

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
-  return <div>Demo</div>;
+  return <section></section>;
 };
 
 export default Demo;
